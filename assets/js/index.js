e5da03387d596183922ee0b9d3db6ac1ff895b68
Brendon <americanbigelow@gmail.com>
2026-02-13 18:40:02 -0600

    Adding plyr

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,9 @@
 // JavaScript files are compiled and minified during the build process to the assets/built folder. See available scripts in the package.json file.
 
+// Import
+import Plyr from 'plyr';
+import 'plyr/dist/plyr.css';
+
 // Import CSS
 import "../css/index.css";
 
@@ -11,3 +15,7 @@ import infiniteScroll from "./infiniteScroll";
 // Call the menu and infinite scroll functions
 menuOpen();
 infiniteScroll();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const player = new Plyr('#audio-player');
+});
